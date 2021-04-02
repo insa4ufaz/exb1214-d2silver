9c27df4c808edb63e2137caca7a369610b797a36
d2silver <>
2021-04-02 12:59:52 +0000

    palindrome test is working

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,11 @@ public class Palindrome{
     public static boolean is_palindrome(String s)
     {
         // add your code here
+        for(int i=0; i<s.length()/2; ++i){
+          if(s.charAt(i) != s.charAt(s.length()-1-i)){
+            return false;
+          }
+        }
         return true;
     }
 }
